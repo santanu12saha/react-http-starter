6f6925c299394f61154e6e0b3c178fd4d9ab4f5f
santanu12saha <saha.santanu0217@gmail.com>
2020-01-05 13:45:38 +0530

    Added Font family and inject with css module configuration

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 
 import Blog from './containers/Blog/Blog';
+import classes from './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div className={classes.App}>
         <Blog />
       </div>
     );
