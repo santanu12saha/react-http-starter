03c4b6e8520d383c5d2c5f03d698afe1513f0cd5
santanu12saha <saha.santanu0217@gmail.com>
2020-01-12 11:30:37 +0530

    Redirecting request.

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import Posts from './Posts/Posts';
 import NewPost from './NewPost/NewPost';
 
-import { Route, NavLink, Switch } from 'react-router-dom';
+import { Route, NavLink, Switch, Redirect } from 'react-router-dom';
 import classes from './Blog.css';
 
 class Blog extends Component {
@@ -35,6 +35,8 @@ class Blog extends Component {
                 <Switch>
                     <Route path="/new-post" component={NewPost}/>
                     <Route path="/posts"  component={Posts}/>
+                    <Redirect from="/" to="/posts"/>
+                    {/*<Route path="/"  component={Posts}/>*/}
                 </Switch>  
             </div>
         );
