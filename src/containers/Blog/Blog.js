7b0caaaf358a8fb8a23e4d9bcfa393c9a3501a5a
santanu12saha <saha.santanu0217@gmail.com>
2020-01-12 12:29:52 +0530

    Adding dummy gaurds for authentication

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -7,6 +7,10 @@ import classes from './Blog.css';
 
 class Blog extends Component {
    
+    state = {
+        auth: false
+    }
+
     render () {
         
         return (
@@ -33,7 +37,7 @@ class Blog extends Component {
                 {/*<Route path="/" exact render={ ()=> <h1>Home</h1>}/>
                 <Route path="/" render={ ()=> <h1>Home2</h1>}/>*/}
                 <Switch>
-                    <Route path="/new-post" component={NewPost}/>
+                    {this.state.auth ? <Route path="/new-post" component={NewPost}/> : null}
                     <Route path="/posts"  component={Posts}/>
                     <Redirect from="/" to="/posts"/>
                     {/*<Route path="/"  component={Posts}/>*/}
